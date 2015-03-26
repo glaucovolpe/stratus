413e99c6f54b9bab74729075485f2a57d8c4126b
Demo User <debian@beaglebone.localdomain>
2015-03-26 16:22:16 +0000

    second commit

--- srcs/GPS/gps.cpp
+++ srcs/GPS/gps.cpp
@@ -1,40 +1,188 @@
-
-#include <stdio.h>   /* Standard input/output definitions */
-#include <string.h>  /* String function definitions */
-#include <unistd.h>  /* UNIX standard function definitions */
-#include <fcntl.h>   /* File control definitions */
-#include <errno.h>   /* Error number definitions */
-#include <termios.h> /* POSIX terminal control definitions */
-
+#include<iostream>
+//#include<stdlib.h>
+#include<cstdlib>
+#include<gps.h>
+#include<string>
+#include<sstream>
 /*
  * 'open_port()' - Open serial port 1.
  *
  * Returns the file descriptor on success or -1 on error.
- */
+*/
+
+using namespace std;
 
 
-int open_port(void)
-{
-  int fd; /* File descriptor for the port */
+GPS::GPS() {
 
-
-  fd = open("/dev/ttyO4", O_RDWR | O_NOCTTY | O_NDELAY);
-  if (fd == -1)
-  {
-   /*
-    * Could not open the port.
-    */
-
-    perror("open_port: Unable to open /dev/ttyf1 - ");
-  }
-  else
-    fcntl(fd, F_SETFL, 0);
-
-  return (fd);
 }
 
- int main (void)
-{
 
-return;
+unsigned int GPS::checkSum(char * data,int sizeOfData){
+    	int checksum = 0;
+    	for(int k=1;k<sizeOfData;k++)
+    	{
+              	checksum ^= data[k];
+		cout<< data[k];
+    	}
+ 
+	cout<<endl;
+//delete[] data;
+    	return(checksum);
+}
+
+int GPS::dataEdit(char * data, int sizeOfData){
+//int j=0;
+int c=0;
+int commas[20];
+//unsigned int check;
+//stringstream ss;
+double number;
+string str;
+
+	for(int i=0; i < sizeOfData; i++){
+		if(data[i]=='$'){
+			c=0;
+			if(data[i+3]=='R'){
+/*
+*	$GPRMC,225446,A,4916.45,N,12311.12,W,000.5,054.7,191194,020.3,E*68
+*
+*	1 = Time of fix 22:54:46 UTC
+*     	2 = Navigation receiver warning A = OK, V = warning
+*     	3 = Latitude 49 deg. 16.45 min 
+*	4 = Lattitude dir  North
+*     	5 = Longitude 123 deg. 11.12 min 
+*	6 = Longitude dir West
+*    	7 = Speed over ground, Knots
+*     	8 = Course Made Good, True
+*     	9 = Date of fix  19 November 1994
+*    	10 = Magnetic variation 20.3 deg East
+*     	11 = mandatory checksum
+*/
+			for(;i<sizeOfData;i++){
+                                        if((data[i]<20) | (i>349)) 
+					break;
+                                        if(data[i]==','){
+                                        commas[c]=i;
+                                        c++;
+                                        }
+                                        if(data[i]=='*'){
+                                        //check = checkSum((data+j),(i-j));
+                                        //cout<<check<<"\n";
+//Tempo
+                                        str.append(data+1,commas[0], commas[1]-commas[0]-1);
+                                        number = atof(str.c_str());
+                                        setTim(number);
+                                        cout<<"Tempo: "<<number<<endl;
+                                        str="";
+//Latitude
+                                        str.append(data+1,commas[1], commas[2]-commas[1]-1);
+                                        number = atof(str.c_str());
+                                        if(data[commas[2]+1]=='N')
+                                        number *= -1;
+                                        setLat(number);
+                                        cout<<"Latitude: "<<getLat()<<endl;
+                                        str="";
+
+//Longitude
+                                        str.append(data+1,commas[3], commas[4]-commas[3]-1);
+                                        number = atof(str.c_str());
+                                        if(data[commas[4]+1]=='E')
+                                        number *= -1;
+                                        setLon(number);
+                                        cout<<"Longitude: "<<getLon()<<endl;
+                                        str="";
+
+//velocidade
+                                        str.append(data+1,commas[6], commas[7]-commas[6]-1);
+                                        number = atof(str.c_str());
+                                        setSpd(number);
+					number = getSpd();
+                                        cout<<"Velocidade: "<<number<<endl;
+                                        str="";
+//direÃ§Ã£o
+                                        str.append(data+1,commas[7], commas[8]-commas[7]-1);
+                                        number = atof(str.c_str());
+                                        setDir(number);
+                                        cout<<"DireÃÃo: "<<number<<endl;
+                                        str="";
+
+
+                                        }
+                                }
+
+			}
+
+			if(data[i+4]=='G'){
+/*
+*	$GPGGA,hhmmss.ss,llll.ll,a,yyyyy.yy,a,x,xx,x.x,x.x,M,x.x,M,x.x,xxxx*hh
+*	1  = UTC of Position
+*	2  = Latitude
+*	3  = N or S
+*	4  = Longitude
+*	5  = E or W
+*	6  = GPS quality indicator (0=invalid; 1=GPS fix; 2=Diff. GPS fix)
+*	7  = Number of satellites in use [not those in view]
+*	8  = Horizontal dilution of position
+*	9  = Antenna altitude above/below mean sea level (geoid)
+*	10 = Meters  (Antenna height unit)
+*	11 = Geoidal separation (Diff. between WGS-84 earth ellipsoid and mean sea level. -=geoid is below WGS-84 ellipsoid)
+*	12 = Meters  (Units of geoidal separation)
+*	13 = Age in seconds since last update from diff. reference station
+*	14 = Diff. reference station ID#
+*	15 = Checksum
+*/
+				for(;i<sizeOfData;i++){
+					if((data[i]<20) | (i>349))
+						break;
+					if(data[i]==','){
+						commas[c]=i;
+						c++;
+					}
+					if(data[i]=='*'){
+					//check = checkSum((data+j),(i-j));
+/*					//cout<<check<<"\n";
+//Tempo
+					str.append(data+1,commas[0], commas[1]-commas[0]-1);
+                                        number = atof(str.c_str());
+                                        setTim(number);
+                                        cout<<"Tempo: "<<number<<endl;
+                                        str="";
+//Latitude
+					str.append(data+1,commas[1], commas[2]-commas[1]-1);
+					number = atof(str.c_str());
+					if(data[commas[2]+1]=='N')
+					number *= -1;
+					setLat(number);
+					cout<<"Latitude: "<<getLat()<<endl;
+					str="";
+//Longitude
+					str.append(data+1,commas[3], commas[4]-commas[3]-1);
+					number = atof(str.c_str());
+					if(data[commas[4]+1]=='E')
+					number *= -1;
+					setLon(number);
+					cout<<"Longitude: "<<getLon()<<endl;
+					str="";
+*/
+//Altitude
+					str.append(data+1,commas[3], commas[2]-commas[1]-1);
+                                        cout << str;
+                                        number = atof(str.c_str());
+                                        setAlt(number);
+
+					}
+				}
+			}
+		//
+		//cout<<endl;
+		}
+	}
+delete[] data;
+return 0;
+}
+
+
+GPS::~GPS(){
+	//GPS's destructor
 }
--- srcs/GPS/gps.h
+++ srcs/GPS/gps.h
@@ -4,8 +4,26 @@
 
 class GPS
 {
+
 private:
-float , LAT, LON, 
+float LATITUDE, LONGITUDE, SPEED, ALTITUDE, DIRECTION, TIME;
+	void setAlt(float alt){ALTITUDE = alt;}
+        void setLat(float lat){LATITUDE = lat;}
+        void setLon(float lon){LONGITUDE = lon;}
+        void setSpd(float spd){SPEED = spd*1.852;}
+	void setTim(float tim){TIME = tim;}
+	void setDir(float dir){DIRECTION = dir;}
+public:
+	GPS();
+	int dataEdit(char * data, int sizeOfData);
+	float getAlt(void){return ALTITUDE;}
+	float getLat(void){return LATITUDE;}
+	float getLon(void){return LONGITUDE;}
+	float getSpd(void){return SPEED;}
+	float getTime(void){return TIME;}
+	unsigned int checkSum(char * data, int sizeOfData);
 
+	virtual ~GPS();
+};
 
-}
+#endif
